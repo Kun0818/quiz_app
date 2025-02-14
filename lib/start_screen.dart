ce36a70586430221af6eddc8fce1c618b0147064
Kun Sie <kun_sie@asus.com>
2025-02-14 10:40:54 +0800

    FEAT: Add quiz and questions screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+  void onPressed() {}
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(148, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: onPressed,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              iconColor: Colors.white,
+              side: const BorderSide(
+                color: Colors.white
+              )
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
+              'Start Quiz',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
